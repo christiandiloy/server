1a69c0e3cea7db09d3c9255b91038c366dd987ef
christiandiloy <diloychristian@gmail.com>
2023-02-19 22:19:04 +0800

    test

--- main.js
+++ main.js
@@ -1,111 +1,107 @@
-// here we get codes from 3rd party plugins/library
-const express = require('express') // server code or to run our own server on localhost specified by port
-const cors = require('cors') // this allows us to access our server on a different domain
-const bodyParser = require("body-parser"); // this allows us to ready request data JSON object
-const app = express() // initialize express server into a variable
-const fs = require('fs') // use file system of windows or other OS to access local files
-const request = require('request');
-const requestAPI = request;
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('Diloy', 'wd32p', '7YWFvP8kFyHhG3eF', {
-    host: '20.211.37.87',
-    dialect: 'mysql'
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const request = require("request");
+const { Sequelize } = require("sequelize");
+
+const app = express();
+const sequelize = new Sequelize("Diloy", "wd32p", "7YWFvP8kFyHhG3eF", {
+  host: "20.211.37.87",
+  dialect: "mysql",
 });
 
-const User = sequelize.define('user', {
+const User = sequelize.define(
+  "user",
+  {
     username: {
-        type: Sequelize.STRING
+      type: Sequelize.STRING,
     },
     full_name: {
-        type: Sequelize.STRING
+      type: Sequelize.STRING,
     },
     password: {
-        type: Sequelize.STRING
+      type: Sequelize.STRING,
     },
     email: {
-        type: Sequelize.STRING
-    }
-},{
-    tableName: 'user',
+      type: Sequelize.STRING,
+    },
+  },
+  {
+    tableName: "user",
     timestamps: false,
-});
+  }
+);
 
-
-
-app.use(cors({
-    exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
+app.use(
+  cors({
+    exposedHeaders: ["Content-Length", "X-Foo", "X-Bar"],
     credentials: true,
-    methods: 'GET,POST,PUT,DELETE',
-    optionsSuccessStatus: 200
-}));
-app.use(bodyParser.urlencoded({ // initialize body parser plugin on express
-    extended: true
-}));
-app.use(bodyParser.json());// initialize body parser plugin on express
+    methods: "GET,POST,PUT,DELETE",
+    optionsSuccessStatus: 200,
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
-let defaultData = [];
-
-app.options('/api/v2/register', cors(), function(req, res) {
-    res.header('Access-Control-Allow-Origin', req.get('Origin'));
-    res.header('Access-Control-Allow-Methods', 'POST');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.sendStatus(200);
+app.use("/api/v2/register", cors(), function (req, res) {
+  res.header("Access-Control-Allow-Origin", req.get("Origin"));
+  res.header("Access-Control-Allow-Methods", "POST");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(200);
 });
 
-
-app.post('/api/v2/register', function (
-    request,
-    response
-) {
-    let retVal = {success: false};
-    console.log('req: ', request.body)
-    User.findOne({
-        where: {
-            username: request.body.username
-        }
-    })
-    .then((result)=>{
-        if(result){
-            retVal.success = false;
-            retVal.message = 'username is already taken'
-            response.send(retVal);
-        }else{
-            User.create({
-                username: request.body.username,
-                password: request.body.password,
-                full_name: request.body.fullName,
-                email: request.body.email
-            })
-                .then((result)=>{
-                    return result.dataValues;
-                })
-                .then((result)=>{
-                    retVal.success = true;
-                    delete result.password;
-                    retVal.userData = null;
-                    // retVal.userData = result; // for auto login after registration
-                })
-                .finally(()=>{
-                    response.send(retVal)
-                })
-                .catch((error)=>{
-                    console.log('error: ', error)
-                })
-        }
-    })
-})
-
-
-const runApp = async ()=>{
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-        app.listen(process.env.PORT || 3000);
- // run app with this given port
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
+app.post("/api/v2/register", function (request, response) {
+  let retVal = { success: false };
+  console.log("req: ", request.body);
+  User.findOne({
+    where: {
+      username: request.body.username,
+    },
+  }).then((result) => {
+    if (result) {
+      retVal.success = false;
+      retVal.message = "username is already taken";
+      response.send(retVal);
+    } else {
+      User.create({
+        username: request.body.username,
+        password: request.body.password,
+        full_name: request.body.fullName,
+        email: request.body.email,
+      })
+        .then((result) => {
+          return result.dataValues;
+        })
+        .then((result) => {
+          retVal.success = true;
+          delete result.password;
+          retVal.userData = null;
+          // retVal.userData = result; // for auto login after registration
+        })
+        .finally(() => {
+          response.send(retVal);
+        })
+        .catch((error) => {
+          console.log("error: ", error);
+        });
     }
-}
-runApp()
+  });
+});
 
+const runApp = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    app.listen(process.env.PORT || 3000);
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+runApp();
