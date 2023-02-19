3b937c997288326cd31336095f8b7ad18edb22c7
christiandiloy <diloychristian@gmail.com>
2023-02-20 01:25:57 +0800

    test

--- main.js
+++ main.js
@@ -38,6 +38,16 @@ app.use(bodyParser.json({ // initialize body parser plugin on express
 }));
 app.use(bodyParser.json());// initialize body parser plugin on express
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'POST');
+      return res.status(200).json({});
+    }
+    next();
+});
+
 let defaultData = [];
 
 app.post('/api/v2/register', function (
