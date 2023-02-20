f71d850520871343803bf6f44a1826514c6d4c97
christiandiloy <diloychristian@gmail.com>
2023-02-20 10:47:34 +0800

    revert

--- main.js
+++ main.js
@@ -38,61 +38,59 @@ app.use(bodyParser.json({ // initialize body parser plugin on express
 }));
 app.use(bodyParser.json());// initialize body parser plugin on express
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://dota2gcashstore.netlify.app"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'POST');
+      return res.status(200).json({});
+    }
     next();
 });
 
-app.options("*", function(req, res, next) {
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-    res.send();
-  });
 let defaultData = [];
 
-app.post('/api/v2/register', async (req, res) => {
-    try {
-        let retVal = {success: false};
-        console.log('req: ', request.body)
-        User.findOne({
-            where: {
-                username: request.body.username
-            }
-        })
-        .then((result)=>{
-            if(result){
-                retVal.success = false;
-                retVal.message = 'username is already taken'
-                response.send(retVal);
-            }else{
-                User.create({
-                    username: request.body.username,
-                    password: request.body.password,
-                    full_name: request.body.fullName,
-                    email: request.body.email
+app.post('/api/v2/register', function (
+    request,
+    response
+) {
+    let retVal = {success: false};
+    console.log('req: ', request.body)
+    User.findOne({
+        where: {
+            username: request.body.username
+        }
+    })
+    .then((result)=>{
+        if(result){
+            retVal.success = false;
+            retVal.message = 'username is already taken'
+            response.send(retVal);
+        }else{
+            User.create({
+                username: request.body.username,
+                password: request.body.password,
+                full_name: request.body.fullName,
+                email: request.body.email
+            })
+                .then((result)=>{
+                    return result.dataValues;
                 })
-                    .then((result)=>{
-                        return result.dataValues;
-                    })
-                    .then((result)=>{
-                        retVal.success = true;
-                        delete result.password;
-                        retVal.userData = null;
-                        // retVal.userData = result; // for auto login after registration
-                    })
-                    .finally(()=>{
-                        response.send(retVal)
-                    })
-                    .catch((error)=>{
-                        console.log('error: ', error)
-                    })
-            }
-        })
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Something went wrong' });
-    }
-  });
+                .then((result)=>{
+                    retVal.success = true;
+                    delete result.password;
+                    retVal.userData = null;
+                    // retVal.userData = result; // for auto login after registration
+                })
+                .finally(()=>{
+                    response.send(retVal)
+                })
+                .catch((error)=>{
+                    console.log('error: ', error)
+                })
+        }
+    })
+})
 
 
 const runApp = async ()=>{
